5be2cfcc0c7ee500e49971c8dec2ccd87ab021b1
Ricardo <josericardocauichmis@gmail.com>
2020-02-21 16:02:09 -0600

    Index de contacto

---
+++ src/triciseguroweb/resources/views/contactos/form.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group {{ $errors->has('nombre') ? 'has-error' : ''}}">
+    <label for="nombre" class="control-label">{{ 'Nombre' }}</label>
+    <input class="form-control" name="nombre" type="text" id="nombre" value="{{ isset($contacto->nombre) ? $contacto->nombre : ''}}" required>
+    {!! $errors->first('nombre', '<p class="help-block">:message</p>') !!}
+</div>
+<div class="form-group {{ $errors->has('numero') ? 'has-error' : ''}}">
+    <label for="numero" class="control-label">{{ 'Numero' }}</label>
+    <textarea class="form-control" rows="5" name="numero" type="textarea" id="numero" required>{{ isset($contacto->numero) ? $contacto->numero : ''}}</textarea>
+    {!! $errors->first('numero', '<p class="help-block">:message</p>') !!}
+</div>
+
+
+<div class="form-group">
+    <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'Create' }}">
+</div>
--- src/triciseguroweb/routes/web.php
+++ src/triciseguroweb/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tarea', 'TareaController');
+Route::resource('contactos', 'ContactosController');
