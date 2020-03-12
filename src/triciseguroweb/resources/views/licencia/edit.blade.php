85b748cd4ed43f947a55262673e42d9a883e5065
Alexdiaz56 <adiazbaas56@gmail.com>
2020-03-12 15:31:54 -0600

    crud modificado

--- src/triciseguroweb/resources/views/licencia/edit.blade.php
+++ src/triciseguroweb/resources/views/licencia/edit.blade.php
@@ -7,7 +7,7 @@
 
             <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header">Edit Licencium #{{ $licencium->id }}</div>
+                    <div class="card-header">Edit Licencia #{{ $licencia->id }}</div>
                     <div class="card-body">
                         <a href="{{ url('/licencia') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                         <br />
@@ -21,7 +21,7 @@
                             </ul>
                         @endif
 
-                        <form method="POST" action="{{ url('/licencia/' . $licencium->id) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                        <form method="POST" action="{{ url('/licencia/' . $licencia->id) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
                             {{ method_field('PATCH') }}
                             {{ csrf_field() }}
 
