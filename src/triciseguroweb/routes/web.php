d27174c6644c17db51f37cf5d88a2eb21737393b
camaramay84 <camaramay84@gmail.com>
2020-03-18 10:11:20 -0600

    Actulizado

--- src/triciseguroweb/routes/web.php
+++ src/triciseguroweb/routes/web.php
@@ -1,15 +1,5 @@
 <?php
   
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,14 +10,7 @@ Route::get('/', function () {
 Route::resource('documentos', 'DocumentosController');
 Route::resource('persona', 'PersonaController');
 Route::resource('queja', 'QuejaController');
-
 Route::resource('licencia', 'LicenciaController');
-
-
 Route::resource('contactos', 'ContactosController');
-
 Route::resource('notificacion', 'NotificacionController');
-
-
-
-Route::resource('calificacion', 'CalificacionController');
+Route::resource('calificaciones', 'CalificacionesController');
