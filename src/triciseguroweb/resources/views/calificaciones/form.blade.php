5c30cf5aa28e6b2c14d79667aee952d28d421fa7
camaramay84 <camaramay84@gmail.com>
2020-03-25 12:24:53 -0600

    bvh

--- src/triciseguroweb/resources/views/calificaciones/form.blade.php
+++ src/triciseguroweb/resources/views/calificaciones/form.blade.php
@@ -1,16 +1,16 @@
 <div class="form-group {{ $errors->has('taxista') ? 'has-error' : ''}}">
     <label for="taxista" class="control-label">{{ 'Taxista' }}</label>
-    <textarea class="form-control" rows="5" name="taxista" type="textarea" id="taxista" >{{ isset($calificacione->taxista) ? $calificacione->taxista : ''}}</textarea>
+    <textarea class="form-control"  name="taxista" type="textarea" id="taxista" >{{ isset($calificacione->taxista) ? $calificacione->taxista : ''}}</textarea>
     {!! $errors->first('taxista', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('servicio') ? 'has-error' : ''}}">
     <label for="servicio" class="control-label">{{ 'Servicio' }}</label>
-    <textarea class="form-control" rows="5" name="servicio" type="textarea" id="servicio" >{{ isset($calificacione->servicio) ? $calificacione->servicio : ''}}</textarea>
+    <textarea class="form-control"  name="servicio" type="textarea" id="servicio" >{{ isset($calificacione->servicio) ? $calificacione->servicio : ''}}</textarea>
     {!! $errors->first('servicio', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('calificacion') ? 'has-error' : ''}}">
     <label for="calificacion" class="control-label">{{ 'Calificacion' }}</label>
-    <textarea class="form-control" rows="5" name="calificacion" type="textarea" id="calificacion" >{{ isset($calificacione->calificacion) ? $calificacione->calificacion : ''}}</textarea>
+    <textarea class="form-control" name="calificacion" type="textarea" id="calificacion" >{{ isset($calificacione->calificacion) ? $calificacione->calificacion : ''}}</textarea>
     {!! $errors->first('calificacion', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('taxista_id') ? 'has-error' : ''}}">
--- src/triciseguroweb/resources/views/persona/index.blade.php
+++ src/triciseguroweb/resources/views/persona/index.blade.php
@@ -9,23 +9,7 @@
 
             <div class="col-md-9">
                 <div class="card">
-<<<<<<< HEAD:src/triciseguroweb/resources/views/calificaciones/index.blade.php
-                    <div class="card-header">Calificaciones</div>
-                    <div class="card-body">
-                        <a href="{{ url('/calificaciones/create') }}" class="btn btn-success btn-sm" title="Add New Calificacione">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
-                        </a>
 
-                        <form method="GET" action="{{ url('/calificaciones') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
-=======
-                    <div class="card-header">Persona</div>
-                    <div class="card-body">
-                        <a href="{{ url('/persona/create') }}" class="btn btn-success btn-sm" title="Add New Persona">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
-                        </a>
-
-                        <form method="GET" action="{{ url('/persona') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
->>>>>>> remotes/origin/Alexis.Kumul:src/triciseguroweb/resources/views/persona/index.blade.php
                             <div class="input-group">
                                 <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
                                 <span class="input-group-append">
@@ -42,52 +26,13 @@
                             <table class="table">
                                 <thead>
                                     <tr>
-<<<<<<< HEAD:src/triciseguroweb/resources/views/calificaciones/index.blade.php
-                                        <th>#</th><th>Taxista</th><th>Servicio</th><th>Calificacion</th><th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($calificaciones as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->taxista }}</td><td>{{ $item->servicio }}</td><td>{{ $item->calificacion }}</td>
-                                        <td>
-                                            <a href="{{ url('/calificaciones/' . $item->id) }}" title="View Calificacione"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/calificaciones/' . $item->id . '/edit') }}" title="Edit Calificacione"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                                            <form method="POST" action="{{ url('/calificaciones' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Calificacione" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-=======
-                                        <th>#</th><th>Nombre</th><th>Apellido</th><th>F Nacimiento</th><th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($persona as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->nombre }}</td><td>{{ $item->apellido }}</td><td>{{ $item->f_nacimiento }}</td>
-                                        <td>
-                                            <a href="{{ url('/persona/' . $item->id) }}" title="View Persona"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/persona/' . $item->id . '/edit') }}" title="Edit Persona"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-
-                                            <form method="POST" action="{{ url('/persona' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Persona" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
->>>>>>> remotes/origin/Alexis.Kumul:src/triciseguroweb/resources/views/persona/index.blade.php
                                             </form>
                                         </td>
                                     </tr>
                                 @endforeach
                                 </tbody>
                             </table>
-<<<<<<< HEAD:src/triciseguroweb/resources/views/calificaciones/index.blade.php
-                            <div class="pagination-wrapper"> {!! $calificaciones->appends(['search' => Request::get('search')])->render() !!} </div>
-=======
-                            <div class="pagination-wrapper"> {!! $persona->appends(['search' => Request::get('search')])->render() !!} </div>
->>>>>>> remotes/origin/Alexis.Kumul:src/triciseguroweb/resources/views/persona/index.blade.php
                         </div>
 
                     </div>
