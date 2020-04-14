6c099ec93a8f6e3a5de98249eaa9cb3be27a33de
Angel Gonzalez <angel.gonzalez.osorio9@gmail.com>
2020-04-14 15:07:11 -0500

    Creacion del APi/PersonaController.php

---
+++ src/triciseguroweb/app/Http/Controllers/Api/PersonaController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $persona = Persona::latest()->paginate(25);
+
+        return $persona;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $persona = Persona::create($request->all());
+
+        return response()->json($persona, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $persona = Persona::findOrFail($id);
+
+        return $persona;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $persona = Persona::findOrFail($id);
+        $persona->update($request->all());
+
+        return response()->json($persona, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Persona::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
