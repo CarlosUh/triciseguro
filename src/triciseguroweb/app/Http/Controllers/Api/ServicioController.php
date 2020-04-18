328b2de7a0046789cfa28c407dbf9e32386fb4db
tlopez <tatianaoe83@gmail.com>
2020-04-17 23:20:49 -0500

    Servicio Rest

---
+++ src/triciseguroweb/app/Http/Controllers/Api/ServicioController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Servicio;
+use Illuminate\Http\Request;
+
+class ServicioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $servicio = Servicio::latest()->paginate(25);
+
+        return $servicio;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $servicio = Servicio::create($request->all());
+
+        return response()->json($servicio, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $servicio = Servicio::findOrFail($id);
+
+        return $servicio;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $servicio = Servicio::findOrFail($id);
+        $servicio->update($request->all());
+
+        return response()->json($servicio, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Servicio::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
